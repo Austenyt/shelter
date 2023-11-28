5cd6bd4910f62f03f82b436e42ad29f774ee1c34
Austenyt <austenyt@gmail.com>
2023-11-28 23:27:01 +0300

    Third commit

--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from dogs.models import Category
+from dogs.models import Category, Dog
 
 
 def index(request):
@@ -15,5 +15,14 @@ def categories(request):
     context = {
         'object_list': Category.objects.all(),
         'title': 'Питомник - Наши породы'
+    }
+    return render(request, 'dogs/categories.html', context)
+
+
+def category_dogs(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Dog.objects.filter(category_id=pk),
+        'title': f'Собаки породы - все наши породы {category_item.name}'
     }
     return render(request, 'dogs/categories.html', context)
