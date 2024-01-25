18341b8096952f59d5a0bae81a566d58e02d27b5
austenyt <austenyt@gmail.com>
2024-01-25 22:27:17 +0300

    forms

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
 
 from dogs.apps import DogsConfig
-from dogs.views import CategoryListView, DogListView, DogCreateView, DogUpdateView, DogDeleteView, IndexView
+from dogs.views import index, categories, DogCreateView, category_dogs
 
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('categories/', CategoryListView.as_view(), name='categories'),
-    path('<int:pk>/dogs/', DogListView.as_view(), name='category'),
-    # path('dogs/create', DogCreateView.as_view(), name='dog_create'),
-    # path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
-    # path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='dog_delete'),
+    path('', index, name='index'),
+    path('categories/', categories, name='categories'),
+    path('<int:pk>/dogs/', category_dogs, name='category_dogs'),
     path('<int:pk>/dogs/create/', DogCreateView.as_view(), name='dog_create')
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,43 +1,24 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, CreateView, UpdateView, TemplateView, DeleteView
+from django.views.generic import CreateView
 
 from dogs.forms import DogForm
 from dogs.models import Category, Dog
 
 
-class IndexView(TemplateView):
-    template_name = 'dogs/index.html'
-    extra_context = {
+def index(request):
+    context = {
+        'object_list': Category.objects.all()[:3],
         'title': 'Питомник - Главная'
     }
-
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        context_data['object_list'] = Category.objects.all()[:3]
-        return context_data
-
-# def index(request):
-#     context = {
-#         'object_list': Category.objects.all()[:3],
-#         'title': 'Питомник - Главная'
-#     }
-#     return render(request, 'dogs/index.html', context)
+    return render(request, 'dogs/index.html', context)
 
 
-# def categories(request):
-#     context = {
-#         'object_list': Category.objects.all(),
-#         'title': 'Питомник - Наши породы'
-#     }
-#     return render(request, 'dogs/category_list.html', context)
-
-
-class CategoryListView(ListView):
-    model = Category
-    extra_context = {
-        'title': 'Питомник - все наши породы'
+def categories(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Питомник - Наши породы'
     }
+    return render(request, 'dogs/categories.html', context)
 
 
 def category_dogs(request, pk):
@@ -47,42 +28,9 @@ def category_dogs(request, pk):
         'category_pk': category_item.pk,
         'title': f'Собаки породы - все наши породы {category_item.name}'
     }
-    return render(request, 'dogs/category_list.html', context)
-
-
-# class DogListView(ListView):
-#     model = Dog
-#
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         queryset = queryset.filter(category_id=self.kwargs.get('pk'))
-#         return queryset
-#
-#     def get_context_data(self, *args, **kwargs):
-#         context_data = super().get_context_data(*args, **kwargs)
-#
-#         category_item = Category.objects.get(pk=self.kwargs.get('pk'))
-#         context_data['category_pk'] = category_item.pk,
-#         context_data['title'] = f'Собаки породы - все наши породы {category_item.name}'
-#
-#         return context_data
+    return render(request, 'dogs/categories.html', context)
 
 
 class DogCreateView(CreateView):
     model = Dog
-    # fields = ('name', 'category',)
-    # successful_url = reverse_lazy('dogs:categories')
     form_class = DogForm
-
-
-class DogUpdateView(UpdateView):
-    model = Dog
-    fields = ('name', 'category',)
-
-    def get_success_url(self):
-        return reverse('dogs:category', args=[self.object.category.pk])
-
-
-class DogDeleteView(DeleteView):
-    model = Dog
-    successful_url = reverse_lazy('dogs:categories')
