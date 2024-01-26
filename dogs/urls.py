7878ea0995ad69816e7fa601c99c39550dfa5946
Austenyt <austenyt@gmail.com>
2024-01-26 12:29:48 +0300

    reqs

--- dogs/urls.py
+++ dogs/urls.py
@@ -9,8 +9,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('categories/', categories, name='categories'),
     path('<int:pk>/dogs/', category_dogs, name='category_dogs'),
-    # path('dogs/create', DogCreateView.as_view(), name='dog_create'),
-    # path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
-    # path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='dog_delete'),
     path('<int:pk>/dogs/create/', DogCreateView.as_view(), name='dog_create')
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -6,17 +6,6 @@ from dogs.forms import DogForm
 from dogs.models import Category, Dog
 
 
-# class IndexView(TemplateView):
-#     template_name = 'dogs/index.html'
-#     extra_context = {
-#         'title': 'Питомник - Главная'
-#     }
-#
-#     def get_context_data(self, **kwargs):
-#         context_data = super().get_context_data(**kwargs)
-#         context_data['object_list'] = Category.objects.all()[:3]
-#         return context_data
-
 def index(request):
     context = {
         'object_list': Category.objects.all()[:3],
@@ -30,14 +19,7 @@ def categories(request):
         'object_list': Category.objects.all(),
         'title': 'Питомник - Наши породы'
     }
-    return render(request, 'dogs/category_list.html', context)
-
-
-# class CategoryListView(ListView):
-#     model = Category
-#     extra_context = {
-#         'title': 'Питомник - все наши породы'
-#     }
+    return render(request, 'dogs/categories.html', context)
 
 
 def category_dogs(request, pk):
@@ -47,42 +29,9 @@ def category_dogs(request, pk):
         'category_pk': category_item.pk,
         'title': f'Собаки породы - все наши породы {category_item.name}'
     }
-    return render(request, 'dogs/category_list.html', context)
-
-
-# class DogListView(ListView):
-#     model = Dog
-#
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         queryset = queryset.filter(category_id=self.kwargs.get('pk'))
-#         return queryset
-#
-#     def get_context_data(self, *args, **kwargs):
-#         context_data = super().get_context_data(*args, **kwargs)
-#
-#         category_item = Category.objects.get(pk=self.kwargs.get('pk'))
-#         context_data['category_pk'] = category_item.pk,
-#         context_data['title'] = f'Собаки породы - все наши породы {category_item.name}'
-#
-#         return context_data
+    return render(request, 'dogs/categories.html', context)
 
 
 class DogCreateView(CreateView):
     model = Dog
-    # fields = ('name', 'category',)
-    # successful_url = reverse_lazy('dogs:categories')
     form_class = DogForm
-
-
-# class DogUpdateView(UpdateView):
-#     model = Dog
-#     fields = ('name', 'category',)
-#
-#     def get_success_url(self):
-#         return reverse('dogs:category', args=[self.object.category.pk])
-#
-#
-# class DogDeleteView(DeleteView):
-#     model = Dog
-#     successful_url = reverse_lazy('dogs:categories')
